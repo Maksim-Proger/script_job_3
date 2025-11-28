db40966d28aa8c688d89b89dc7aed18d6f2dde94
Maksim <i@mpozdnjakov.ru>
2025-11-28 11:45:17 +0300

    ww

--- api_client.py
+++ api_client.py
@@ -23,13 +23,43 @@ def send_api_request(server_host: str, server_port: int, action: str, file_path:
         return
 
     try:
-        requests.post(url, headers=headers, data=json.dumps(data), timeout=5)
+        response = requests.post(url, headers=headers, data=json.dumps(data), timeout=5)
+        response.raise_for_status()
+        mo_name = os.path.splitext(os.path.basename(file_path))[0]
+        log_data = None
+
+        if action in ("update", "new"):
+
+            log_data = {
+                "commit": [{
+                    "path": file_path,
+                    "mo": mo_name,
+                    "status": True,
+                    "status-text": "success"
+                }]
+            }
+        elif action == "delete":
+            log_data = {
+                "delete": [{
+                    "mo": mo_name,
+                    "status": True,
+                    "status-text": "success"
+                }]
+            }
+
         logger.info(
-            "action=%s path=%s target=%s:%s",
-            action, file_path, server_host, server_port
+            "action=%s result=%s target=%s:%s",
+            action, json.dumps(log_data), server_host, server_port
         )
+
+    except requests.HTTPError as e:
+        logger.error(
+            "action=%s http_error=%s status_code=%s target=%s:%s",
+            action, e, getattr(e.response, "status_code", None), server_host, server_port
+        )
+
     except Exception as e:
         logger.error(
-            "action=%s path=%s target=%s:%s error=%s",
-            action, file_path, server_host, server_port, e
+            "action=%s exception=%s target=%s:%s",
+            action, e, server_host, server_port, exc_info=True
         )
